d91429dcf364773848bc641d8c4d4defbfaf5a77
Chris Manson <chris@manson.ie>
2023-07-19 02:50:17 +0100

    fix test

--- addon/modifiers/reveal.js
+++ addon/modifiers/reveal.js
@@ -2,10 +2,11 @@ import { modifier } from 'ember-modifier';
 import Reveal from 'reveal.js';
 import Markdown from 'reveal.js/plugin/markdown/markdown.esm.js';
 import RevealHighlight from 'reveal.js/plugin/highlight/highlight.js';
+import RevealNotes from 'reveal.js/plugin/notes/notes.js';
 
 export default modifier(function reveal(/*element, positional, named*/) {
   let deck = new Reveal({
-    plugins: [Markdown, RevealHighlight],
+    plugins: [Markdown, RevealHighlight, RevealNotes],
   });
   deck.initialize({
     transition: 'none',
